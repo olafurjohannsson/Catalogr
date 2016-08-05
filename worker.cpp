8165cffcc047533638848bedbc21b401a15e4132
Ólafur Jóhannsson <olafurjohannss@gmail.com>
2016-08-05 16:08:20 +0000

    worker logic with thread

--- worker.cpp
+++ worker.cpp
@@ -1,6 +1,46 @@
 #include "worker.h"
 
-worker::worker()
+Worker::Worker()
 {
 
 }
+
+Worker::Worker(QTextBrowser *txtBrowser, QString text) {
+    this->txtBrowser = txtBrowser;
+    this->text = text;
+/*
+    this->thread = new QThread();
+    this->moveToThread(this->thread);
+    connect(this, SIGNAL(error(QString)), this, SLOT(errorString(QString)));
+    connect(this->thread, SIGNAL(started()), this, SLOT(process()));
+    connect(this, SIGNAL(finished()), this->thread, SLOT(quit()));
+    connect(this, SIGNAL(finished()), this, SLOT(deleteLater()));
+    connect(this->thread, SIGNAL(finished()), this->thread, SLOT(deleteLater()));
+    this->thread->start();
+    */
+}
+
+Worker::~Worker() {
+    //delete this->txtBrowser;
+}
+
+void Worker::process() {
+    qDebug() << "process";
+
+    QDirIterator iterator(QDir(this->text), QDirIterator::Subdirectories);
+    qint64 size = 0, files = 0;
+    while (iterator.hasNext()) {
+        QString item = iterator.next();
+        QFileInfo innerFile(item);
+        if (innerFile.isFile()) {
+            files++;
+            size += innerFile.size();
+        }
+    }
+
+    this->txtBrowser->setText(QString("%1 files, Size: %2 bytes").arg(files).arg(size));
+}
+
+//void Worker::finished() { }
+
+//void Worker::error(QString err) { }
--- worker.h
+++ worker.h
@@ -1,12 +1,34 @@
 #ifndef WORKER_H
 #define WORKER_H
 
-#include <QObject>
 
-class worker
+#include <QObject>
+#include <QString>
+#include <QDir>
+#include <QDirIterator>
+#include <QTextBrowser>
+#include <QDebug>
+
+class Worker : public QObject
 {
+    Q_OBJECT
+
 public:
-    worker();
+    Worker();
+    Worker(QTextBrowser*, QString);
+    ~Worker();
+
+public slots:
+    void process();
+
+signals:
+    void finished();
+    void error(QString err);
+
+private:
+    QTextBrowser *txtBrowser;
+    QString text;
+    QThread *thread;
 };
 
 #endif // WORKER_H
